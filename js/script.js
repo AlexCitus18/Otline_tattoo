1816ffd2a1f0ad464e7d71dad41f7cb6ea6910bb
AlexCitus18 <Alex.Citrus@yandex.ru>
2023-05-29 02:17:09 +0300

    + FAQ

--- js/script.js
+++ js/script.js
@@ -112,3 +112,33 @@ autoPlay();
 carousel.addEventListener("scroll", infiniteScroll);
 wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
+
+/* -----------/СЛАЙДЕР-----------*/
+
+/* -----------Accordion-----------*/
+
+const titles = document.querySelectorAll('.accordion__title');
+const contents = document.querySelectorAll('.accordion__content');
+
+    titles.forEach(item => item.addEventListener('click', () => {
+        const activeContent = document.querySelector('#' + item.dataset.tab);
+
+        if (activeContent.classList.contains('active')) {
+            activeContent.classList.remove('active');
+            item.classList.remove('active');
+            activeContent.style.maxHeight = 0;
+        } else {
+            contents.forEach(element => {
+                element.classList.remove('active');
+                element.style.maxHeight = 0;
+            });
+
+            titles.forEach(element => element.classList.remove('active'));
+
+            item.classList.add('active');
+            activeContent.classList.add('active');
+            activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+        }
+    }))
+
+/* -----------/Accordion-----------*/
