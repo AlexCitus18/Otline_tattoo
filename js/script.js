60a3604e2788f741ff83c20c061095a054fbf3a0
AlexCitus18 <Alex.Citrus@yandex.ru>
2023-06-09 13:26:23 +0300

    all components

--- js/script.js
+++ js/script.js
@@ -176,3 +176,48 @@ filterContainer.addEventListener("click", (event) =>{
  });
 
  /* -----------/Gallery-----------*/
+
+/*----------загрузка файлов (форма обратной связи)------- */
+
+ let inputs = document.querySelectorAll('.input__file');
+  Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+      labelVal = label.querySelector('.input__file-button-text').innerText;
+
+    input.addEventListener('change', function (e) {
+      let countFiles = '';
+      if (this.files && this.files.length >= 1)
+        countFiles = this.files.length;
+
+      if (countFiles)
+        label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+      else
+        label.querySelector('.input__file-button-text').innerText = labelVal;
+    });
+  });
+
+/*---------/загрузка файлов------- */
+
+/*---------выбор одного checkbox------- */
+
+inputs=document.getElementsByTagName("input");
+for(var i=0;i<inputs.length;i++)
+	{
+	if(inputs[i].type=="checkbox")
+		{
+		inputs[i].onchange=function()
+			{
+			inputs=document.getElementsByTagName("input");
+			for(var i=0;i<inputs.length;i++)
+				{
+				if(inputs[i].type=="checkbox")
+					{
+					inputs[i].checked=false;
+					}
+					this.checked=true;
+				}
+			}
+		}
+	}
+
+/*---------/выбор одного checkbox------- */
