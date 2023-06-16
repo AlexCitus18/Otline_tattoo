48579c7efe76fd42bffec8c53f499e1e91583c35
AlexCitus18 <Alex.Citrus@yandex.ru>
2023-06-16 11:08:44 +0300

    -

--- js/script.js
+++ js/script.js
@@ -24,22 +24,7 @@ if (menu && menuBtn) {
     })
 }
 
-/* плавный скролл по ссылкам */
 
-/* const anchors = document.querySelectorAll('a[href*="#"]');
-
-anchors.forEach(anchor => {
-	anchor.addEventListener('click', event => {
-		event.preventDefault();
-
-		const blockID = anchor.getAttribute('href').substring(1);
-
-		document.getElementById(blockID).scrollIntoView({
-			behavior: 'smooth',
-			block: 'start'
-		})
-	})
-}) */
 
 
 /* -----------СЛАЙДЕР-----------*/
